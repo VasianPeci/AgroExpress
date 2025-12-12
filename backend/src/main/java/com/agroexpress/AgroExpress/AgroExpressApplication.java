7fc7d04e80812333b99db4cd1e1445fa1c9665ef
VasianPeci <vasianpeci26@gmail.com>
2025-12-12 15:47:51 +0100

    AgroExpress Project Structure

---
+++ backend/src/main/java/com/agroexpress/AgroExpress/AgroExpressApplication.java
@@ -0,0 +1,13 @@
+package com.agroexpress.AgroExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroExpressApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/entity/Roles.java
@@ -0,0 +1,9 @@
+package entity;
+
+public enum Roles {
+    ROLE_USER,          // Default User / Buyer (Blerës)
+    ROLE_SELLER,        // Shitës
+    ROLE_TRANSPORTER,       // Transportues
+    ROLE_MANAGER,       // Menaxher
+    ROLE_ADMIN          // Administrator
+}
