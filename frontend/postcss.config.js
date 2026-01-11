6e106634a1aff3407d6e21e7f89111b26527f1ea
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2026-01-11 17:45:09 +0100

    Delete frontend directory

--- frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindCompat from '@tailwindcss/postcss7-compat';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [
-    tailwindCompat,
-    autoprefixer,
-  ]
-};
